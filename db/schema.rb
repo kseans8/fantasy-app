0c0358feeb9aa11c20aad1b90f560c87e52f9c2c
kseans8 <kseans8@gmail.com>
2017-01-30 15:34:36 -0500

    added create player migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130195759) do
+ActiveRecord::Schema.define(version: 20170130203228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "leagues", force: :cascade do |t|
     t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.integer  "goals"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
