980a83cb7d341158bc1d77629c5c59c1c947ae04
kseans8 <kseans8@gmail.com>
2017-01-30 15:08:07 -0500

    added create team migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130195403) do
+ActiveRecord::Schema.define(version: 20170130195759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 20170130195403) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name",                   null: false
+    t.integer  "rank",                   null: false
+    t.integer  "score",      default: 0, null: false
+    t.integer  "user_id",                null: false
+    t.integer  "league_id",              null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["league_id"], name: "index_teams_on_league_id", using: :btree
+    t.index ["user_id"], name: "index_teams_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
