1a18a90dd77bf8d783df3e4b95bbe9bc311a2664
kseans8 <kseans8@gmail.com>
2017-01-31 14:33:46 -0500

    setup routes and changed name of home_controller.rb to league_controller.rb (left over config issues from boilerplate switch over

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
-  root "home#index"
-  resources :leagues, only: [:index]
+
+  root "league#index"
+  
+  resources :leagues, only: [:index, :show] do
+    resources :teams, only: [:index, :show] do
+      resources :players, only: [:index, :show]
+    end
+  end
+
+  resources :users do
+    resources :teams do
+      resources :players, only: [:index, :show]
+    end
+  end
 end
