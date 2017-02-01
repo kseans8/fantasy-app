cac14918e19117343860a5d3ad85b1b5096d7502
kseans8 <kseans8@gmail.com>
2017-02-01 11:57:18 -0500

    created react components for some of the routes

---
+++ react/src/components/LeagueList.js
@@ -0,0 +1,19 @@
+import React, { Component }  from 'react';
+
+class LeagueList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+
+    return(
+      <div>
+        there will be a list containing leagues here
+      </div>
+    )
+  }
+}
+
+export default LeagueList;
---
+++ react/src/components/TeamList.js
@@ -0,0 +1,19 @@
+import React, { Component }  from 'react';
+
+class TeamList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+
+    return(
+      <div>
+        there will be a list containing teams here
+      </div>
+    )
+  }
+}
+
+export default TeamList;
