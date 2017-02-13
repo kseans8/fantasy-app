0180d7a7a278e3c8fe0f3fa760dbd961ff91559f
kseans8 <kseans8@gmail.com>
2017-02-13 13:06:55 -0500

    codeclimate cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email])
-    end
+  end
 end
